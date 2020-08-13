afbe4960464aac7e49cb22011bbe71e7b5f2b93b
Savke <savkeren97@gmail.com>
2020-08-13 21:07:28 +0200

    room ui

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoomComponent } from './room/room.component';
 import { RoomsComponent } from './room/rooms/rooms.component';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { RoomsComponent } from './room/rooms/rooms.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/room/room.component.ts
+++ front/src/app/room/room.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RoomComponent implements OnInit {
 
   constructor() { }
-
+  proba = [0, 1, 2, 3];
   ngOnInit(): void {
   }
 
