ba80ce7fc3b44c2d80997eaa64d41a1f974c4631
Savke <savkeren97@gmail.com>
2020-08-13 14:26:55 +0200

    room

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomComponent } from './room/room.component';
+import { RoomsComponent } from './room/rooms/rooms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
