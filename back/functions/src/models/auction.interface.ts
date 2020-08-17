8da06bf303febca451ba4e6db866533395347afe
Milica Cakic <mcakic97@gmail.com>
2020-08-17 11:37:22 +0200

    Dodati neki modeli

---
+++ back/functions/src/models/auction.interface.ts
@@ -0,0 +1,7 @@
+import Room from './room.interface';
+
+interface Auction {
+    rooms: Room[];
+}
+
+export default Auction;
---
+++ back/functions/src/models/flight.interface.ts
@@ -0,0 +1,10 @@
+interface Flight {
+    origin: string;
+    destination: string;
+    numberBC: number;
+    numberEC: number;
+    soldSeatsBC: number;
+    travelDate: string;  // Date(travelDate)
+}
+
+export default Flight;
