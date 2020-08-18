8cb587c19a8bdf279ef503e93c95d9313bcffd6c
AndrejaZi <andrejaziv1997@gmail.com>
2020-08-18 14:48:55 +0200

    Dodao responsive za stranu sa sovama za klijenta

--- front/src/app/components/admin-room/admin-room.component.ts
+++ front/src/app/components/admin-room/admin-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServisService } from 'src/app/servis.service';
 
 @Component({
   selector: 'app-admin-room',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminRoomComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public service: ServisService) {}
   ngOnInit(): void {
   }
 
